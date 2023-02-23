39af82ec270c38c3a08fbbd9951118f2c0830c0c
IsmailovWD <ismailovwd@gmail.com>
2023-02-23 19:49:04 +0500

    23.02.2023

--- seeders/20221110064230-user.js
+++ seeders/20221110064230-user.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('user', [
       {
         firstname: "Dastruchi",
@@ -10,11 +10,19 @@ module.exports = {
         password: "$2a$08$sJjS/l.Mfz/jcSfEDRivQOrBx0qJ5lezAy1dE.eLN351BSg68lPmq",
         deleted_at: 0,
         role: "dasturchi",
+      },
+      {
+        firstname: "Admin",
+        lastname: "Admin",
+        phone_number: '+998111111111',
+        password: "$2a$08$sJjS/l.Mfz/jcSfEDRivQOrBx0qJ5lezAy1dE.eLN351BSg68lPmq",
+        deleted_at: 0,
+        role: "Admin",
       }
     ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('user', null, {});
   }
 };
